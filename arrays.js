a7b65df481abaab669a449e80432573645cd421d
Nguyen Thuan <ndt0153@gmail.com>
2017-04-06 19:19:56 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,4 +1,44 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittlest"];
 function addElementToBeginningOfArray(array, element) {
-  return array.push(element)
+  var temp = array;
+  temp.unshift(element);
+  return temp;
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var temp = array;
+  temp.unshift(element);
+  return temp;
+}
+function addElementToEndOfArray(array, element) {
+  var temp = array;
+  temp.push(element);
+  return temp;
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  var temp = array;
+  temp.push(element);
+  return temp;
+}
+function accessElementInArray(array, index) {
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  var temp = array;
+  temp.shift();
+  return temp;
+}
+function removeElementFromBeginningOfArray(array) {
+//  var temp = array;
+//  temp.slice(0);
+  //return temp;
+  return array.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var temp = array;
+  temp.pop();
+  return temp;
+}
+function removeElementFromEndOfArray(array) {
+  var temp = array.slice(0, array.length - 1);
+  return temp;
 }
