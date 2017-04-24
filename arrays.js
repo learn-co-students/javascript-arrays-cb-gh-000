5fd58214e3b41eac84007504deddc4939b698af3
taylor watson <taylorwatson18@icloud.com>
2017-04-24 02:43:45 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -13,3 +13,33 @@ addElementToBeginningOfArray = (array, element) => {
 destructivelyAddElementToBeginningOfArray = (array, element) => {
   return array.unshift(element)
 };
+
+addElementToEndOfArray = (array, element) => {
+  const anotherArray = [array, ...element]
+  return anotherArray;
+}
+
+destructivelyAddElementToEndOfArray = (array, element) => {
+  return array.push(element);
+}
+
+accessElementInArray = (array, index) => {
+  return array[index];
+}
+
+destructivelyRemoveElementFromBeginningOfArray = (array) => {
+  array.shift()
+  return array;
+}
+
+removeElementFromBeginningOfArray = (array) => {
+  return array.slice(1);
+}
+
+destructivelyRemoveElementFromEndOfArray = (array) => {
+  return array.pop();
+}
+
+removeElementFromEndOfArray = (array) => {
+  return array.slice(-1);
+}
