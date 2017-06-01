095db892f9b1be2a4ffefca4d592f612c353d3a3
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 18:00:57 -0400

    Create removeElementFromBeginningOfArray function

--- arrays.js
+++ arrays.js
@@ -26,3 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
     array.shift();
     return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+    return array.slice(1);
+}
