a39738495023d0b34da7f87332e5497b9ddd5671
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 17:54:38 -0400

    Fix typo in addElementToEndOfArray function

--- arrays.js
+++ arrays.js
@@ -10,5 +10,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-    return [array, ...element];
+    return [...array, element];
 }
