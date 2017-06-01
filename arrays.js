85af64b9ed515e020724a9ae32e430b3718abab0
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 17:51:10 -0400

    Create addElementToEndOfArray function

--- arrays.js
+++ arrays.js
@@ -8,3 +8,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element);
     return array;
 }
+
+function addElementToEndOfArray(array, element) {
+    return [array, ...element];
+}
