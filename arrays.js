a056566bf3c30cd147c244645e71f9b342b990fd
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 17:58:37 -0400

    Create destructivelyRemoveElementFromBeginningOfArray function

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
     return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+    array.shift();
+    return array;
+}
