608b30693ed74c77eb558f7111d5667166d96abf
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 17:56:15 -0400

    Create destructivelyAddElementToEndOfArray function

--- arrays.js
+++ arrays.js
@@ -12,3 +12,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function addElementToEndOfArray(array, element) {
     return [...array, element];
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element);
+    return array;
+}
