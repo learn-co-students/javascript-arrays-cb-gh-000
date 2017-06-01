449d11017814db0c3951d89829cec66e218f29e3
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 18:01:44 -0400

    Create destructivelyRemoveElementFromEndOfArray function

--- arrays.js
+++ arrays.js
@@ -30,3 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
     return array.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop();
+    return array;
+}
