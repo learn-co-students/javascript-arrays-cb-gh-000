1a80b91409558eb894489f05ec96c499070d0031
Sergiy  Tkachuk <awertg@mail.ua>
2017-09-05 16:13:32 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+  var evenNumbers = new Array(3);
+  element = "vadim tupit";
+function addElementToBeginningOfArray(array, element) {
+
+  array.unshift(`${element}`);
+  console.log(array);
+}
+addElementToBeginningOfArray(evenNumbers, "dfsffh");
