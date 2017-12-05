9d2541629288794784b79a51ed0f8820115230c6
Ahmad Taufiq Hidayat <ataufiq665@gmail.com>
2017-12-05 04:35:22 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,43 @@
-chocolateBars=[]
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
-function addElementToBeginningOfArray(array,element){
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
 
-  chocolateBars.unshift([array])
-  chocolateBars.unshift(element)
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function removeElementFromEndOfArray(array){
+  var temp = array.slice(0, array.length-1);
+  return temp;
+}
+
+function removeElementFromBeginningOfArray(array){
+  var temp = array.slice(1);
+  return temp;
 }
