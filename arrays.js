6a13ff45815d926d350aef0c6bbc1c32f4f927e5
Ruben Vargas <rubenbustos33@gmail.com>
2018-03-18 23:11:42 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+
+function addElementToBeginningOfArray(arr, elem){
+  arr.unshift(elem);
+  return arr;
+}
+
+
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+
+}
+
+function addElementToEndOfArray(){
+
+}
+
+function destructivelyAddElementToEndOfArray(){
+
+}
