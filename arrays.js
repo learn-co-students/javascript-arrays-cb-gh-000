bb6b96c3fc78afcd6a1c8e87677a800efcdddf44
Diep Thuy <boychaos9x@gmail.com>
2020-04-27 15:37:41 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars=[ 'snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray(arr, element) {
+return (element,...arr);
+
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+return  arr.unshift(element);
+}
+function addElementToEndOfArray(arr, element){
+return (...arr, element);
+}
+function destructivelyAddElementToEndOfArray(arr, element)= {
+return arr.push(element);
+}
+function removeElementFromBeginningOfArray(arr){
+   return arr.slide(1);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+  return arrCopy.shift();
+}
